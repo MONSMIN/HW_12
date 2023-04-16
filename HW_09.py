a310dce423bdc080bb54446c3fb1d7989a06042c
MONSMIN <99141331+MONSMIN@users.noreply.github.com>
2023-04-16 13:45:30 +0300

    first commit

---
+++ HW_09.py
@@ -0,0 +1,179 @@
+from classes import AddressBook, Record, Name, Phone, Birthday
+from datetime import date
+import pickle
+
+contacts = AddressBook()
+
+
+def input_error(func):
+    
+    def inner(*args):
+        try:
+            return func(*args)
+        except IndexError:
+            return "Not enough params. Print help"
+        except KeyError:
+            return 'Contact not found, try again or use help'
+        except AttributeError:
+            return 'Not enough params. Print help'
+    return inner
+
+
+@input_error
+def add(*args):
+    list_of_param = args[0].split()
+    name = list_of_param[0]
+    birthday = None
+    phone_numbers = []
+    for param in list_of_param[1:]:
+        if len(param) == 12 and param.isnumeric():
+            phone_numbers.append(Phone(param))
+        else:
+            birthday = Birthday(param)
+
+    record = Record(Name(name), phone_numbers, birthday)
+    contacts.add_record(record)
+
+    if not birthday:
+        return f'{name}, {phone_numbers[0]}'
+    return f'{name},{phone_numbers[0]},{birthday}'
+
+
+def show_all(*args, contacts=contacts):
+    if not contacts.data:
+        return 'No contacts'
+    
+    contact_list = []
+    for name, record in contacts.data.items():
+        phones = ', '.join(str(phone) for phone in record.phone)
+        if record.birthday:
+            contact_list.append(f"{name} {phones} days to birthday: {record.days_to_birthday()}")
+        else:
+            contact_list.append(f"{name} {phones}, Date of birth is not specified")
+
+    page = args[0]
+
+    if not page:
+        return "\n".join(contact_list)
+    
+    for records in contacts.iterator(int(page)):
+        for name, record in records:
+            phones = ', '.join(str(phone) for phone in record.phone)
+            days_to_birthday = f"days to birthday: {record.days_to_birthday()}" if record.birthday else ""
+            print(f"{name} {phones} {days_to_birthday}")
+        print('*' * 100)
+    
+
+@input_error
+def phone(*args):
+    list_of_param = args[0].split()
+    name = Name(list_of_param[0])
+    phone_number = [Phone(phone) for phone in list_of_param[1:-1]]
+    record = contacts.get(name.value)
+    phone_number = record.phone[0]
+    return f'{phone_number}'
+
+
+@input_error
+def change(*args):
+    list_of_param = args[0].split()
+    name = Name(list_of_param[0])
+    phone_number = [Phone(phone) for phone in list_of_param[1:]]
+    record = contacts.get(name.value)
+    if not record:
+        raise KeyError
+    if not phone_number:
+        raise AttributeError
+    record.phone = phone_number
+    return f'Contact {name.value} updated {str(phone_number[0])}'
+
+
+def save(contacts, filename="contacts.bin"):
+    with open(filename, "wb") as f:
+        pickle.dump(contacts, f)
+    print(f"Contacts saved to {filename}")
+
+def load(filename="contacts.bin"):
+    with open(filename, 'rb') as f:
+        contacts = pickle.load(f)
+    print(f"Contacts loaded from {filename}")
+    return contacts
+
+
+def exit(*args):
+    return "Good bye!"
+
+
+def no_command(*args):
+    return 'Unknown command, try again or help'
+
+
+def help(*args):
+    return "If there are problems, read the file, readme!"
+
+
+def hello(*args):
+    return "How can I help you?"
+
+
+COMMANDS = {help: 'help',
+            hello: 'hello',
+            add: 'add',
+            show_all: 'show all',
+            phone: 'phone',
+            change: 'change',
+            exit: 'exit',
+            save: 'save',
+            load: 'load'}
+
+
+def command_handler(text):
+    for command, kword in COMMANDS.items():
+        if text.startswith(kword):
+            return command, text.replace(kword, '').strip()
+    return no_command, None
+
+
+
+
+def main():
+    print('Hello user!')
+    while True:
+        
+        user_input = input('>>>')
+        command, data = command_handler(user_input)
+
+        print(command(data))
+    
+
+        if user_input == 'exit':
+            save(contacts) 
+            break 
+            
+
+if __name__ == '__main__':
+
+    record = Record(Name("Marina"), [Phone("380993456789")], Birthday("15.05.1995"))
+    record1 = Record(Name("Petro"), [Phone("380977654321")], Birthday("10.10.1988"))
+    record2 = Record(Name("Olena"), [Phone("380994567890")], Birthday("20.06.1987"))
+    record3 = Record(Name("Serg"), [Phone("380956789012")], Birthday("07.12.1990"))
+    record4 = Record(Name("Lydmila"), [Phone("380983456789")], Birthday("23.08.1992"))
+    record5 = Record(Name("Sanya"), [Phone("380963210987")], Birthday("02.03.1983"))
+    record6 = Record(Name("Vasya"), [Phone("380977890123")], Birthday("18.09.1998"))
+    record7 = Record(Name("Oleg"), [Phone("380954321098")], Birthday("25.11.1985"))
+    record8 = Record(Name("Tanya"), [Phone("380979012345")], Birthday("12.07.1991"))
+    
+    
+    contacts.add_record(record)
+    contacts.add_record(record1)
+    contacts.add_record(record2)
+    contacts.add_record(record3)
+    contacts.add_record(record4)
+    contacts.add_record(record5)
+    contacts.add_record(record6)
+    contacts.add_record(record7)
+    contacts.add_record(record8)
+
+
+    main()
+    
---
+++ classes.py
@@ -0,0 +1,129 @@
+from collections import UserDict
+from datetime import datetime, timedelta, date
+from itertools import islice
+import pickle
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return self.value
+    
+    def __repr__(self):
+        return str(self)
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        self._value = value
+    
+ 
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def __str__(self):
+        return "Name: " + super().__str__()
+
+
+class Phone(Field):
+    def __init__(self, phone):
+        super().__init__(phone)
+
+    def __str__(self):
+        return "Phone: " + super().__str__()
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, phone):
+        if phone.isnumeric() and len(phone) == 12:
+            self._value = phone
+        else:
+            raise ValueError("Phone number must contain only 12 digits.")
+
+
+class Birthday(Field):
+    def __init__(self, value=None):
+        super().__init__(value)
+
+    def __str__(self):
+        return "Birthday: " + (super().__str__() if self.value else '')
+
+    def  value(self, value):
+        try:
+            datetime.strptime(value, '%d.%m.%Y')
+        except ValueError:
+            raise ValueError("Incorrect date format, should be dd.mm.yyyy")
+        self._value = value
+
+
+class Record:
+    def __init__(self, name, phone, birthday=None):
+        self.name = name
+        self.phone = phone
+        self.birthday = birthday
+        
+    def add_phone(self, phone:Phone):
+        self.phone.append(phone)
+        
+    def del_phone(self, phone):
+        self.phone.remove(phone)
+        
+    def edit_phone(self, old_phone, new_phone):
+        index = self.phone.index(old_phone)
+        self.phone[index] = new_phone
+
+    def add_birthday(self, birthday):
+        if isinstance(birthday, Birthday):
+            self.birthday = birthday
+        else:
+            raise ValueError("Invalid birthday")
+        
+
+    def days_to_birthday(self):
+        if not self.birthday or not self.birthday.value:
+            return None
+        
+        today = datetime.today()
+        bday = datetime.strptime(self.birthday.value, "%d.%m.%Y").replace(year=today.year)
+        
+        if bday < today:
+            bday = bday.replace(year=today.year + 1)
+        
+        delta = bday - today
+        return delta.days
+
+
+class AddressBook(UserDict):
+    
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def iterator(self, page=None):
+        start = 0
+        while True:
+            result = list(islice(self.data.items(), start, start + page))
+            if not result:
+                break
+            yield result
+            start += page
+
+    def save_to_file(self):
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load_from_file(cls, filename):
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
+            
+            
+
+
